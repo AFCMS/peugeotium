bade3b8b3bfbdd53fd389dfb09dbc09afe45a17c
AFCMS <afcm.contact@gmail.com>
2022-07-25 21:05:09 +0200

    Fixes

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-local S = minetest.get_translator("peugeotnium")
+local S = minetest.get_translator("peugeotium")
 
-minetest.register_node("peugeotnium:stone_with_peugeotnium", {
-	description = S("Peugeotnium Ore"),
-	_doc_items_longdesc = S("Some peugeotnium contained in stone, it is prety common and can be found below sea level."),
-	tiles = { "peugeotnium_peugeotnium_ore.png" },
+minetest.register_node("peugeotium:stone_with_peugeotium", {
+	description = S("Peugeotium Ore"),
+	_doc_items_longdesc = S("Some peugeotium contained in stone, it is prety common and can be found below sea level."),
+	tiles = { "peugeotium_peugeotium_ore.png" },
 	is_ground_content = true,
 	groups = { pickaxey = 4, building_block = 1, material_stone = 1, blast_furnace_smeltable = 1 },
 	sounds = mcl_sounds.node_sound_stone_defaults(),
@@ -12,18 +12,18 @@ minetest.register_node("peugeotnium:stone_with_peugeotnium", {
 	_mcl_silk_touch_drop = true,
 })
 
-minetest.register_craftitem("peugeotnium:peugeotnium_ingot", {
-	description = S("Peugeotnium Ingot"),
-	_doc_items_longdesc = S("Molten peugeotnium. It is used to craft armor, tools, and whatnot."),
-	inventory_image = "peugeotnium_peugeotnium_ingot.png",
+minetest.register_craftitem("peugeotium:peugeotium_ingot", {
+	description = S("Peugeotium Ingot"),
+	_doc_items_longdesc = S("Molten peugeotium. It is used to craft armor, tools, and whatnot."),
+	inventory_image = "peugeotium_peugeotium_ingot.png",
 	groups = { craftitem = 1 },
 })
 
 
-minetest.register_node("peugeotnium:peugeotniumblock", {
-	description = S("Block of Peugeotnium"),
-	_doc_items_longdesc = S("A block of peugeotnium is mostly a decorative block but also useful as a compact storage of peugeotnium ingots."),
-	tiles = { "peugeotnium_peugeotnium_block.png" },
+minetest.register_node("peugeotium:peugeotiumblock", {
+	description = S("Block of Peugeotium"),
+	_doc_items_longdesc = S("A block of peugeotium is mostly a decorative block but also useful as a compact storage of peugeotium ingots."),
+	tiles = { "peugeotium_peugeotium_block.png" },
 	is_ground_content = false,
 	groups = { pickaxey = 4, building_block = 1 },
 	sounds = mcl_sounds.node_sound_metal_defaults(),
@@ -31,28 +31,30 @@ minetest.register_node("peugeotnium:peugeotniumblock", {
 	_mcl_hardness = 5,
 })
 
+mcl_stairs.register_stair_and_slab_simple("peugeotium", "peugeotium:peugeotiumblock", S("Peugeotium Stairs"), S("Peugeotium Slab"), S("Double Peugeotium Slab"))
+
 minetest.register_craft({
 	type = "cooking",
-	output = "peugeotnium:peugeotnium_ingot",
-	recipe = "peugeotnium:stone_with_peugeotnium",
+	output = "peugeotium:peugeotium_ingot",
+	recipe = "peugeotium:stone_with_peugeotium",
 	cooktime = 10,
 })
 
 
 minetest.register_craft({
-	output = "peugeotnium:peugeotniumblock",
+	output = "peugeotium:peugeotiumblock",
 	recipe = {
-		{ "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot" },
-		{ "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot" },
-		{ "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot" },
+		{ "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot" },
+		{ "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot" },
+		{ "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot" },
 	},
 })
 
 minetest.register_craft({
-	output = "peugeotnium:peugeotnium_ingot 9",
+	output = "peugeotium:peugeotium_ingot 9",
 	recipe = {
-		{ "peugeotnium:peugeotniumblock" },
-	}
+		{ "peugeotium:peugeotiumblock" },
+	},
 })
 
 local stonelike = { "mcl_core:stone", "mcl_core:diorite", "mcl_core:andesite", "mcl_core:granite" }
@@ -60,7 +62,7 @@ local stonelike = { "mcl_core:stone", "mcl_core:diorite", "mcl_core:andesite", "
 
 minetest.register_ore({
 	ore_type       = "scatter",
-	ore            = "peugeotnium:stone_with_peugeotnium",
+	ore            = "peugeotium:stone_with_peugeotium",
 	wherein        = stonelike,
 	clust_scarcity = 10000,
 	clust_num_ores = 3,
@@ -71,7 +73,7 @@ minetest.register_ore({
 
 minetest.register_ore({
 	ore_type       = "scatter",
-	ore            = "peugeotnium:stone_with_peugeotnium",
+	ore            = "peugeotium:stone_with_peugeotium",
 	wherein        = stonelike,
 	clust_scarcity = 5000,
 	clust_num_ores = 2,
@@ -82,7 +84,7 @@ minetest.register_ore({
 
 minetest.register_ore({
 	ore_type       = "scatter",
-	ore            = "peugeotnium:stone_with_peugeotnium",
+	ore            = "peugeotium:stone_with_peugeotium",
 	wherein        = stonelike,
 	clust_scarcity = 10000,
 	clust_num_ores = 4,
@@ -92,10 +94,10 @@ minetest.register_ore({
 })
 
 
-minetest.register_tool("peugeotnium:sword_peugeotnium", {
-	description = S("Peugeotnium Sword"),
+minetest.register_tool("peugeotium:sword_peugeotium", {
+	description = S("Peugeotium Sword"),
 	_doc_items_longdesc = S("Swords are great in melee combat, as they are fast, deal high damage and can endure countless battles. Swords can also be used to cut down a few particular blocks, such as cobwebs."),
-	inventory_image = "peugeotnium_peugeotniumsword.png",
+	inventory_image = "peugeotium_peugeotiumsword.png",
 	wield_scale = mcl_vars.tool_wield_scale,
 	groups = { weapon = 1, sword = 1, dig_speed_class = 5, enchantability = 30 },
 	tool_capabilities = {
@@ -105,7 +107,7 @@ minetest.register_tool("peugeotnium:sword_peugeotnium", {
 		punch_attack_uses = 4000,
 	},
 	sound = { breaks = "default_tool_breaks" },
-	_repair_material = "peugeotnium:peugeotnium_ingot",
+	_repair_material = "peugeotium:peugeotium_ingot",
 	_mcl_toollike_wield = true,
 	_mcl_diggroups = {
 		swordy = { speed = 8, level = 5, uses = 4000 },
@@ -115,19 +117,19 @@ minetest.register_tool("peugeotnium:sword_peugeotnium", {
 
 
 minetest.register_craft({
-	output = "peugeotnium:sword_peugeotnium",
+	output = "peugeotium:sword_peugeotium",
 	recipe = {
-		{ "peugeotnium:peugeotnium_ingot" },
-		{ "peugeotnium:peugeotnium_ingot" },
+		{ "peugeotium:peugeotium_ingot" },
+		{ "peugeotium:peugeotium_ingot" },
 		{ "mcl_core:stick" },
 	}
 })
 
 
-minetest.register_tool("peugeotnium:pick_peugeotnium", {
-	description = S("Peugeotnium Pickaxe"),
+minetest.register_tool("peugeotium:pick_peugeotium", {
+	description = S("Peugeotium Pickaxe"),
 	_doc_items_longdesc = S("Pickaxes are mining tools to mine hard blocks, such as stone. A pickaxe can also be used as weapon, but it is rather inefficient."),
-	inventory_image = "peugeotnium_peugeotniumpick.png",
+	inventory_image = "peugeotium_peugeotiumpick.png",
 	wield_scale = mcl_vars.tool_wield_scale,
 	groups = { tool = 1, pickaxe = 1, dig_speed_class = 6, enchantability = 30 },
 	tool_capabilities = {
@@ -138,7 +140,7 @@ minetest.register_tool("peugeotnium:pick_peugeotnium", {
 		punch_attack_uses = 5000,
 	},
 	sound = { breaks = "default_tool_breaks" },
-	_repair_material = "peugeotnium:sword_peugeotnium",
+	_repair_material = "peugeotium:sword_peugeotium",
 	_mcl_toollike_wield = true,
 	_mcl_diggroups = {
 		pickaxey = { speed = 50, level = 6, uses = 5000 }
@@ -146,19 +148,19 @@ minetest.register_tool("peugeotnium:pick_peugeotnium", {
 })
 
 minetest.register_craft({
-	output = "peugeotnium:pick_peugeotnium",
+	output = "peugeotium:pick_peugeotium",
 	recipe = {
-		{ "peugeotnium:sword_peugeotnium", "peugeotnium:sword_peugeotnium", "peugeotnium:sword_peugeotnium" },
+		{ "peugeotium:sword_peugeotium", "peugeotium:sword_peugeotium", "peugeotium:sword_peugeotium" },
 		{ "", "mcl_core:stick", "" },
 		{ "", "mcl_core:stick", "" },
 	}
 })
 
-minetest.register_tool("peugeotnium:shovel_peugeotnium", {
-	description = S("Peugeotnium Shovel"),
+minetest.register_tool("peugeotium:shovel_peugeotium", {
+	description = S("Peugeotium Shovel"),
 	_doc_items_longdesc = S("Shovels are tools for digging coarse blocks, such as dirt, sand and gravel. They can also be used to turn grass blocks to grass paths. Shovels can be used as weapons, but they are very weak."),
 	_doc_items_usagehelp = S("To turn a grass block into a grass path, hold the shovel in your hand, then use (rightclick) the top or side of a grass block. This only works when there's air above the grass block."),
-	inventory_image = "peugeotnium_peugeotniumshovel.png",
+	inventory_image = "peugeotium_peugeotiumshovel.png",
 	wield_scale = mcl_vars.tool_wield_scale,
 	groups = { tool = 1, shovel = 1, dig_speed_class = 6, enchantability = 30 },
 	tool_capabilities = {
@@ -169,7 +171,7 @@ minetest.register_tool("peugeotnium:shovel_peugeotnium", {
 	},
 	on_place = minetest.registered_items["mcl_tools:shovel_diamond"].on_place,
 	sound = { breaks = "default_tool_breaks" },
-	_repair_material = "peugeotnium:peugeotnium_ingot",
+	_repair_material = "peugeotium:peugeotium_ingot",
 	_mcl_toollike_wield = true,
 	_mcl_diggroups = {
 		shovely = { speed = 50, level = 6, uses = 5000 }
@@ -177,18 +179,18 @@ minetest.register_tool("peugeotnium:shovel_peugeotnium", {
 })
 
 minetest.register_craft({
-	output = "peugeotnium:shovel_peugeotnium",
+	output = "peugeotium:shovel_peugeotium",
 	recipe = {
-		{ "peugeotnium:peugeotnium_ingot" },
+		{ "peugeotium:peugeotium_ingot" },
 		{ "mcl_core:stick" },
 		{ "mcl_core:stick" },
 	}
 })
 
-minetest.register_tool("peugeotnium:axe_peugeotnium", {
-	description = S("Peugeotnium Axe"),
+minetest.register_tool("peugeotium:axe_peugeotium", {
+	description = S("Peugeotium Axe"),
 	_doc_items_longdesc = axe_longdesc,
-	inventory_image = "peugeotnium_peugeotniumaxe.png",
+	inventory_image = "peugeotium_peugeotiumaxe.png",
 	wield_scale = mcl_vars.tool_wield_scale,
 	groups = { tool = 1, axe = 1, dig_speed_class = 6, enchantability = 30 },
 	tool_capabilities = {
@@ -199,7 +201,7 @@ minetest.register_tool("peugeotnium:axe_peugeotnium", {
 	},
 	on_place = minetest.registered_items["mcl_tools:axe_diamond"].on_place,
 	sound = { breaks = "default_tool_breaks" },
-	_repair_material = "peugeotnium:peugeotnium_ingot",
+	_repair_material = "peugeotium:peugeotium_ingot",
 	_mcl_toollike_wield = true,
 	_mcl_diggroups = {
 		axey = { speed = 50, level = 6, uses = 5000 }
@@ -207,27 +209,27 @@ minetest.register_tool("peugeotnium:axe_peugeotnium", {
 })
 
 minetest.register_craft({
-	output = "peugeotnium:axe_peugeotnium",
+	output = "peugeotium:axe_peugeotium",
 	recipe = {
-		{ "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot" },
-		{ "peugeotnium:peugeotnium_ingot", "mcl_core:stick" },
+		{ "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot" },
+		{ "peugeotium:peugeotium_ingot", "mcl_core:stick" },
 		{ "", "mcl_core:stick" },
 	},
 })
 
 minetest.register_craft({
-	output = "peugeotnium:axe_peugeotnium",
+	output = "peugeotium:axe_peugeotium",
 	recipe = {
-		{ "peugeotnium:peugeotnium_ingot", "peugeotnium:peugeotnium_ingot" },
-		{ "mcl_core:stick", "peugeotnium:peugeotnium_ingot" },
+		{ "peugeotium:peugeotium_ingot", "peugeotium:peugeotium_ingot" },
+		{ "mcl_core:stick", "peugeotium:peugeotium_ingot" },
 		{ "mcl_core:stick", "" },
 	},
 })
 
 
 mcl_armor.register_set({
-	name = "peugeotnium",
-	description = "Peugeotnium",
+	name = "peugeotium",
+	description = "Peugeotium",
 	durability = 2000,
 	enchantability = 30,
 	points = {
@@ -237,7 +239,7 @@ mcl_armor.register_set({
 		feet = 6,
 	},
 	toughness = 2,
-	craft_material = "peugeotnium:peugeotnium_ingot",
+	craft_material = "peugeotium:peugeotium_ingot",
 	sound_equip = "mcl_armor_equip_diamond",
 	sound_unequip = "mcl_armor_unequip_diamond",
 })
